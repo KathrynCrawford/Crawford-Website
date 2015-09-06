e727db12e2c68152f33591355e7a04358c2ce197
Kathrynwatts <kathrynjwatts@gmail.com>
2015-09-05 19:15:42 -0500

    Text updates

---
+++ js/crawford.js
@@ -0,0 +1,15 @@
+<script language="JavaScript">
+
+$(document).ready(function() {
+    $(".jumper").on("click", function(e) {
+
+        e.preventDefault();
+
+        $("body, html").animate({ 
+            scrollTop: $( $(this).attr('href') ).offset().top 
+        }, 600);
+
+    });
+});
+
+</script>
